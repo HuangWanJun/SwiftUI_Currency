61bde53a1a08bb348c68592bb3d246bcd6a15e7e
Samuel Huang <samuel.huang@tonghaifinancial.com>
2020-11-26 22:37:59 +0800

    Initial Commit

---
+++ SwiftUI_Currency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Currency
+//
+//  Created by Samuel Huang on 26/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Currency/SwiftUI_CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CurrencyApp.swift
+//  SwiftUI_Currency
+//
+//  Created by Samuel Huang on 26/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
